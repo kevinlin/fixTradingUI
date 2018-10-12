fe74abeba50dafc2144d281b913a6ba665fbd57e
Kevin Lin <kevin.lin@zuhlke.com>
2018-10-12 22:29:32 +0800

    Remove obsolete properties xxxLotsPerUnit

--- src/app/model/trading-strategy.ts
+++ src/app/model/trading-strategy.ts
@@ -10,37 +10,31 @@ export class TradingStrategy {
   // Contract details
   contract1Symbol: string;
   contract1Coefficient: number;
-  contract1LotsPerUnit: number;
   contract1Side: OrderSide;
   contract1Lots: number;
 
   contract2Symbol: string;
   contract2Coefficient: number;
-  contract2LotsPerUnit: number;
   contract2Side: OrderSide;
   contract2Lots: number;
 
   contract3Symbol: string;
   contract3Coefficient: number;
-  contract3LotsPerUnit: number;
   contract3Side: OrderSide;
   contract3Lots: number;
 
   contract4Symbol: string;
   contract4Coefficient: number;
-  contract4LotsPerUnit: number;
   contract4Side: string;
   contract4Lots: number;
 
   contract5Symbol: string;
   contract5Coefficient: number;
-  contract5LotsPerUnit: number;
   contract5Side: string;
   contract5Lots: number;
 
   contract6Symbol: string;
   contract6Coefficient: number;
-  contract6LotsPerUnit: number;
   contract6Side: string;
   contract6Lots: number;
 
