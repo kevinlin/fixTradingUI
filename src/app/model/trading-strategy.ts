5b34b3e659dac6d2fffd55e68e862a1cc93bda55
Kevin Lin <kevin.lin@zuhlke.com>
2018-10-23 13:40:03 +0800

    Remove contrat?Side properties from TradingStrategy

--- src/app/model/trading-strategy.ts
+++ src/app/model/trading-strategy.ts
@@ -1,5 +1,4 @@
 import { Direction } from './enum/direction.enum';
-import { OrderSide } from './enum/order-side.enum';
 import { StrategyState } from './enum/strategy-state.enum';
 
 export class TradingStrategy {
@@ -10,32 +9,26 @@ export class TradingStrategy {
   // Contract details
   contract1Symbol: string;
   contract1Coefficient: number;
-  contract1Side: OrderSide;
   contract1Lots: number;
 
   contract2Symbol: string;
   contract2Coefficient: number;
-  contract2Side: OrderSide;
   contract2Lots: number;
 
   contract3Symbol: string;
   contract3Coefficient: number;
-  contract3Side: OrderSide;
   contract3Lots: number;
 
   contract4Symbol: string;
   contract4Coefficient: number;
-  contract4Side: string;
   contract4Lots: number;
 
   contract5Symbol: string;
   contract5Coefficient: number;
-  contract5Side: string;
   contract5Lots: number;
 
   contract6Symbol: string;
   contract6Coefficient: number;
-  contract6Side: string;
   contract6Lots: number;
 
   constantFactor: number;
