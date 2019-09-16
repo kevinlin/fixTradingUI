1dd5aa18e6fdafd418ada3ef11b3320dded95bc3
Kevin Lin <kevin.lin@zuhlke.com>
2019-09-16 23:42:58 +0800

    Remove .pipe(takeUntil(componentDestroyed(this))) for all notification observables in BasePageComponent

--- src/app/page-component/base-page-component.ts
+++ src/app/page-component/base-page-component.ts
@@ -1,8 +1,6 @@
-import {ApplicationRef, OnDestroy} from '@angular/core';
+import {ApplicationRef} from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {Message} from '@stomp/stompjs';
-import {componentDestroyed} from '@w11k/ngx-componentdestroyed';
-import {takeUntil} from 'rxjs/operators';
 
 import {AppComponent} from '../app.component';
 import {AlertDialogComponent} from '../component/alert-dialog/alert-dialog.component';
@@ -10,39 +8,33 @@ import {Notification} from '../model/notification';
 import {StompClientService} from '../service/stomp-client.service';
 import {ToastsManager} from '../toast/toasts-manager.service';
 
-export class BasePageComponent implements OnDestroy {
+export class BasePageComponent {
   public loading: boolean;
 
   constructor(protected stompClient: StompClientService, protected toastr: ToastsManager, protected appRef: ApplicationRef, protected dialog: MatDialog) {
     this.toastr.setRootViewContainerRef((appRef.components[0].instance as AppComponent).viewRef);
   }
 
-  ngOnDestroy(): void {
-  }
-
   protected baseOnInit() {
     try {
-      this.stompClient.notificationObservable.pipe(takeUntil(componentDestroyed(this)))
-        .subscribe((message: Message) => {
-          const notification: Notification = JSON.parse(message.body);
-          this.toastr.info(notification.message, notification.action);
+      this.stompClient.notificationObservable.subscribe((message: Message) => {
+        const notification: Notification = JSON.parse(message.body);
+        this.toastr.info(notification.message, notification.action);
+      });
+      this.stompClient.warningObservable.subscribe((message: Message) => {
+        const notification: Notification = JSON.parse(message.body);
+        this.toastr.warning(notification.message, notification.action);
+      });
+      this.stompClient.alertObservable.subscribe((message: Message) => {
+        const notification: Notification = JSON.parse(message.body);
+        this.toastr.error(notification.message, notification.action);
+        const dialogRef = this.dialog.open(AlertDialogComponent, {
+          data: notification
         });
-      this.stompClient.warningObservable.pipe(takeUntil(componentDestroyed(this)))
-        .subscribe((message: Message) => {
-          const notification: Notification = JSON.parse(message.body);
-          this.toastr.warning(notification.message, notification.action);
+        dialogRef.afterClosed().subscribe(result => {
+          console.log(`Dialog is closed with result: ${result}`);
         });
-      this.stompClient.alertObservable.pipe(takeUntil(componentDestroyed(this)))
-        .subscribe((message: Message) => {
-          const notification: Notification = JSON.parse(message.body);
-          this.toastr.error(notification.message, notification.action);
-          const dialogRef = this.dialog.open(AlertDialogComponent, {
-            data: notification
-          });
-          dialogRef.afterClosed().subscribe(result => {
-            console.log(`Dialog is closed with result: ${result}`);
-          });
-        });
+      });
     } catch (err) {
       console.log('baseOnInit()->', err);
     }
